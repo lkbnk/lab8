90bfb1e0a3011eb48dc8a1283b7ef4713683ac66
Ilya <yakubenko@complexis.biz>
2023-05-30 15:08:30 +0300

    task3 commit

--- 3.py
+++ 3.py
@@ -1,9 +1,9 @@
 from PIL import Image, ImageDraw, ImageFont
 
-n = input("Введите имя получателя: ")
-im = Image.open('8_1.jpg')
-f = ImageFont.truetype("arial_bold.ttf", 20)
-draw = ImageDraw.Draw(im)
-draw.text((100,85), n + ", поздравляю, " , font = f, fill = "#E6399B")
-im.show()
-im.save(n + 'otkr.png')
+name = input("Кого хотите поздравить: ")
+image = Image.open('8_1.jpg')
+font = ImageFont.truetype("arial_bold.ttf", 22)
+draw = ImageDraw.Draw(image)
+draw.text((110,80), name + ", поздравляю, " , font = font, fill = "#FF00FF")
+image.show()
+image.save(name + 'card.png')
